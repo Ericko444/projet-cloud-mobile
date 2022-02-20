673dd030736cfebbfb85f3e3f46364d76e403ae7
Ericko Manarivo <manarivoericko4@gmail.com>
2022-02-20 14:28:28 +0300

    notifications

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -6,15 +6,37 @@ import {
   IonToolbar,
 } from '@ionic/react'
 import ExploreContainer from '../components/ExploreContainer'
-import MarginHeader from '../components/MarginHeader'
 import './Notifications.css'
+import SockJs from 'sockjs-client'
+import { Client, Message, Stomp } from '@stomp/stompjs'
 
+const SOCKET_URL = 'https://projet-cloud-signal.herokuapp.com/our-websocket'
 const Notifications: React.FC = () => {
- 
+  const socket = new SockJs(SOCKET_URL)
+  var stompClient = Stomp.over(socket)
+  stompClient.connect({}, onConnected)
+  function onConnected() {
+    console.log("Connected");
+  }
+  function onError(){
+    console.log("Error");
+  }
+  stompClient.onConnect = function (frame) {
+    console.log("In on connect");
+    stompClient.subscribe('/topic/private-messages/User-15', callback);
+  };
+  stompClient.activate();
+  var callback = function (message:Message) {
+    // called when the client receives a STOMP message from the server
+    if (message.body) {
+      alert('got message with body ' + message.body);
+    } else {
+      alert('got empty message');
+    }
+  };
   return (
     <IonPage>
       <IonContent fullscreen>
-      <MarginHeader></MarginHeader>
         <ExploreContainer name="Notifications page" />
       </IonContent>
     </IonPage>
