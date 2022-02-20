75b2995c522a745d8b1e5b5f2ee6fce920731709
Ericko Manarivo <manarivoericko4@gmail.com>
2022-02-20 14:24:17 +0300

    socket impl

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,13 @@
 import { IonAvatar, IonContent, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import MarginHeader from '../components/MarginHeader';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar className='toolbar'>
-          <IonItem lines='none' className='tool-item'>
-          <IonAvatar slot='start'>
-            <img src='https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg'></img>
-          </IonAvatar>
-          <IonLabel>
-            <p>Hello,</p>
-            <h3>RAKOTO Jean</h3>
-          </IonLabel>
-        </IonItem>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
+        <MarginHeader></MarginHeader>
         <IonHeader>
           <IonToolbar>
             <IonTitle>Vos signalements</IonTitle>
--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -1,15 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Notifications.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
+import ExploreContainer from '../components/ExploreContainer'
+import MarginHeader from '../components/MarginHeader'
+import './Notifications.css'
 
 const Notifications: React.FC = () => {
+ 
   return (
     <IonPage>
       <IonContent fullscreen>
+      <MarginHeader></MarginHeader>
         <ExploreContainer name="Notifications page" />
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default Notifications;
+export default Notifications
